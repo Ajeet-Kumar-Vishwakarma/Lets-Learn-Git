79b1ee93be9a8c3277b88d7083cafc2f36f9d7d7
Ajeet Kumar Vishwakarma <h20230060@goa.bits-pilani.ac.in>
2024-01-23 02:14:31 +0530

    added message in sum.java

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ class sum{
     public static void main(String[] args){
         int a=4;
         int b=3;
-        System.out.println(a+b);
+        System.out.println("modified sum of java"+a+b);
     }
 }
